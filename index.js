f08bc5d2420330ee345eeeedbf116a430c78c15b
Gabriela Berecka <gabrielaberecka@gmail.com>
2020-05-11 22:32:38 +0100

    change listen port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-var port_number = server.listen(process.env.PORT || 5000);
+let port_number = process.env.PORT || 5000;
 app.listen(port_number, () => {
   console.log('webgl-daftacademy on 5000!')
 });
